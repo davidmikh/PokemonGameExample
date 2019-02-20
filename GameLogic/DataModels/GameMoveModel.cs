3c88922f3e85d08cd9477f60c08c408fa4dd130a
David Mikhayelyan <david.mikhayelyan@outlook.com>
2019-02-19 22:31:06 -0800

    Added the data models back to gamelogic

---
+++ GameLogic/DataModels/GameMoveModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLogic.DataModels
+{
+    public class GameMoveModel
+    {
+        public string Name { get; }
+        public int MaxPP { get; }
+        public int CurrentPP { get; set; }
+        public int Power { get; }
+
+        public GameMoveModel(string Name, int MaxPP, int Power)
+        {
+            this.Name = Name;
+            this.MaxPP = MaxPP;
+            this.CurrentPP = MaxPP;
+            this.Power = Power;
+        }
+    }
+}
